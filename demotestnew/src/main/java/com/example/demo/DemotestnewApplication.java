3cf5c63522232cb0c383e64fa0792f554967c5fc
ASUS <you@example.com>
2021-12-24 07:42:17 +0530

    Now commit to git is OK ..

--- demotestnew/src/main/java/com/example/demo/DemotestnewApplication.java
+++ demotestnew/src/main/java/com/example/demo/DemotestnewApplication.java
@@ -2,11 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 @SpringBootApplication
 public class DemotestnewApplication {
+	
+	@RequestMapping("/admin")
+	public String hello() {
+		System.out.println("welcome");
+		return "<h1>welome back</h1>" ;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemotestnewApplication.class, args);
